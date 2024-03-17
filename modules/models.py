d00e963b353baf9777f699d930b56d6b35052cee
dmitriyalushkin <pantific37@mail.ru>
2024-03-17 16:25:44 +0300

    Создание приложения modules

--- modules/models.py
+++ modules/models.py
@@ -3,3 +3,23 @@ from django.db import models
 class Module(models.Model):
     name = models.CharField(max_length=50, verbose_name='название модуля')
     description = models.TextField(verbose_name='описание модуля')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'модуль'
+        verbose_name_plural = 'модули'
+
+
+class Lesson(models.Model):
+    module = models.ForeignKey(Module, on_delete=models.CASCADE, verbose_name='модуль')
+    name = models.CharField(max_length=50, verbose_name='название урока')
+    description = models.TextField(verbose_name='описание урока')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
